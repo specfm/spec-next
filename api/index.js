3283e74f092b2b2cd33e604de91e53b19bf117c4
Brian Lovin <briandlovin@gmail.com>
2018-07-24 11:51:03 -0700

    Allow cors on the api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 // @flow
 require('now-env')
+const microCors = require('micro-cors')
 const fetch = require('node-fetch');
 const API_URL_ROOT = 'https://api.simplecast.com/v1'
 const API_KEY = process.env.SIMPLECAST_API_KEY
+const cors = microCors()
 
-module.exports = async (req) => {
+// $FlowFixMe
+const handler = async (req) => {
   const response = await fetch(`${API_URL_ROOT}${req.url}`, {
     method: "GET",
     headers: {
@@ -14,3 +17,5 @@ module.exports = async (req) => {
 	const json = await response.json();
 	return json;
 };
+
+module.exports = cors(handler)
