a56b14643b3c05ef01bde864a044a6e99765b9c6
Brian Lovin <briandlovin@gmail.com>
2018-07-24 19:03:55 -0700

    Extend theme

--- components/theme/index.js
+++ components/theme/index.js
@@ -1,6 +1,20 @@
 export const theme = {
+  bg: {
+    wash: '#F6F7F8',
+    default: '#FFF'
+  },
+  border: {
+    default: '#D6DCE7',
+    active: '#C9CED8'
+  },
   text: {
     default: '#000',
-    alt: "#FF0000"
+    secondary: '#212325',
+    tertiary: '#5C6166',
+    placeholder: '#91989F',
+  },
+  brand: {
+    default: '#067AE4',
+    alt: '#0684F8'
   }
 }
