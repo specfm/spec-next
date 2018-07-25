f10bf211550c4d2f68c3d354f17973660fcc38d2
Brian Lovin <briandlovin@gmail.com>
2018-07-24 19:05:10 -0700

    Temporary about page

---
+++ pages/about/index.js
@@ -0,0 +1,19 @@
+// @flow
+import * as React from 'react'
+import Page from '../../components/Page'
+import { SectionHeading, Heading, Subheading } from '../../components/Page'
+
+export default class About extends React.Component<{}> {
+  render() {
+    return (
+      <Page>
+        <SectionHeading>
+          <Heading>About Us</Heading>
+          <Subheading>
+            
+          </Subheading>
+        </SectionHeading>
+      </Page>
+    )
+  }
+}
