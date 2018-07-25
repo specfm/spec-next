e16295beac34ae2b74c308c6b885c3ac25b7c1a4
Brian Lovin <briandlovin@gmail.com>
2018-07-24 19:04:46 -0700

    Podcast card

--- components/PodcastCard/index.js
+++ components/PodcastCard/index.js
@@ -1,8 +1,26 @@
 // @flow
 import * as React from 'react'
+import { Link as RouteLink } from '../../config/routes'
+import type { ConfigPodcast } from '../../types'
+import Card from '../Card'
+import { Art } from './style'
 
-export default class PodcastCard extends React.Component<{}> {
+type Props = {
+  podcast: ConfigPodcast
+}
+
+export default class PodcastCard extends React.Component<Props> {
   render() {
-    return null
+    const { podcast } = this.props
+
+    return (
+      <RouteLink key={podcast.id} route='podcasts' params={{ slug: podcast.slug }}>
+        <a>
+          <Card>
+            <Art src={podcast.artworkUrl} />
+          </Card>
+        </a>
+      </RouteLink>
+    )
   }
 }
---
+++ components/PodcastCard/style.js
@@ -0,0 +1,9 @@
+// @flow
+import styled from 'styled-components'
+
+export const Art = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+  margin-bottom: -6px;
+`
