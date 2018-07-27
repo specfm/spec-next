b400ecc33dfff26f285c2c2902391e71f1f52624
Brian Lovin <briandlovin@gmail.com>
2018-07-26 18:29:48 -0700

    Protect api cors, send invalid url error codes

--- servers/api/index.js
+++ servers/api/index.js
@@ -1,12 +1,26 @@
 // @flow
 require('now-env')
 const microCors = require('micro-cors')
+const { send } = require('micro')
 const fetch = require('node-fetch');
 const API_URL_ROOT = 'https://api.simplecast.com/v1'
 const API_KEY = process.env.SIMPLECAST_API_KEY
-const cors = microCors()
+const cors = microCors({
+  origin:
+    process.env.NODE_ENV === 'production'
+      ? [
+          'https://spec.fm',
+          /specfm-next-(\w|-)+\.now\.sh/g,
+          /spec\.fm$/,
+        ]
+      : 'http://localhost:3000',
+  credentials: true,
+})
 
-const handler = async (req) => {
+const handler = async (req, res) => {
+  if (!req.url) return send(res, 400, { error: 'Invalid url'})
+  if (!req.url.startsWith('/podcasts')) return send(res, 400, { error: 'Invalid url'})
+
   const response = await fetch(`${API_URL_ROOT}${req.url}`, {
     method: "GET",
     headers: {
