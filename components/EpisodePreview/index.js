88b9dabf177c498386ca92652e345a1a50800d97
Brian Lovin <briandlovin@gmail.com>
2018-07-27 15:51:41 -0700

    Load audio players just in time for improved perf on episode views

--- components/EpisodePreview/index.js
+++ components/EpisodePreview/index.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react'
 import type { SimplecastEpisode, ConfigPodcast } from '../../types'
+import VisibilitySensor from 'react-visibility-sensor'
 import { Link as RouteLink } from '../../config/routes'
 import { Grid, Title, Timestamp,Divider } from './style'
 import { getDateObject } from '../../lib/getDateObject'
@@ -11,30 +12,48 @@ type Props = {
   podcast: ConfigPodcast
 }
 
-class EpisodePreview extends React.Component<Props> {
+type State = {
+  audioPlayerVisible: boolean
+}
+
+class EpisodePreview extends React.Component<Props, State> {
+  state = { audioPlayerVisible: false }
+
+  displayAudioPlayer = (visibility: boolean) => {
+    return this.setState({ audioPlayerVisible: visibility })
+  }
   render() {
     const { episode, podcast } = this.props
+    const { audioPlayerVisible } = this.state
     const { month, year, day } = getDateObject(episode.published_at)
     const datestring = `${month} ${day}, ${year}`
 
     return (
-      <Grid>
-        <RouteLink route='episode' params={{ slug: podcast.slug, episodeId: episode.id }}>
-          <a>
-            <Timestamp alt={datestring}>{datestring}</Timestamp>
-          </a>
-        </RouteLink>
+      <VisibilitySensor onChange={this.displayAudioPlayer} partialVisibility={true}>
+        <Grid>
+          <RouteLink route='episode' params={{ slug: podcast.slug, episodeId: episode.id }}>
+            <a>
+              <Timestamp alt={datestring}>{datestring}</Timestamp>
+            </a>
+          </RouteLink>
 
-        <RouteLink route='episode' params={{ slug: podcast.slug, episodeId: episode.id }}>
-          <a>
-            <Title>{episode.title}</Title>
-          </a>
-        </RouteLink>
+          <RouteLink route='episode' params={{ slug: podcast.slug, episodeId: episode.id }}>
+            <a>
+              <Title>{episode.title}</Title>
+            </a>
+          </RouteLink>
 
-        <Markdown>{episode.description}</Markdown>
-        <audio src={episode.audio_url} controls preload="none"></audio>
-        <Divider />
-      </Grid>
+          <Markdown>{episode.description}</Markdown>
+
+          {
+            audioPlayerVisible
+            ? <audio src={episode.audio_url} controls preload="none"></audio>
+            : <div style={{ height: '66px'}} />
+          }
+
+          <Divider />
+        </Grid>
+      </VisibilitySensor>
     )
   }
 }
