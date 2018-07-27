11dd48ac8d8152c5d1ac60af06655349d130606a
Brian Lovin <briandlovin@gmail.com>
2018-07-27 12:37:32 -0700

    Move retired pocasts to bottom of list

--- components/PodcastsGrid/index.js
+++ components/PodcastsGrid/index.js
@@ -13,15 +13,19 @@ class PodcastsGrid extends React.Component<Props> {
   render() {
     const { podcasts } = this.props
     const layout = config.find(podcast => podcast && podcast.slug === 'layout')
+    const sortedPodcasts = [363, 1034, 1386, 1684, 2128, 2693, 4211, 1332, 1457, 2070]
+      .map(id => podcasts.find(podcast => podcast.id === id))
+    const renderedPodcasts = [...sortedPodcasts, layout] 
+    
 
     return (
       <Container>
         <Grid>
           {
-            podcasts.map(podcast => {
-              const configPodcast = api.getConfigPodcastFromId(podcast.id)
+            renderedPodcasts.map(podcast => {
+              const configPodcast = podcast && podcast.id && api.getConfigPodcastFromId(podcast.id)
               if (!configPodcast) return null
-              return <PodcastCard key={podcast.id} podcast={configPodcast} />
+              return <PodcastCard key={configPodcast.id} podcast={configPodcast} />
             })
           }
 
