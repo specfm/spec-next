596b4918d0f3c1739a13504f9d11f762fc620a63
Brian Lovin <briandlovin@gmail.com>
2018-07-27 17:43:29 -0700

    Fix episode view overflow

--- components/EpisodeShareButtons/style.js
+++ components/EpisodeShareButtons/style.js
@@ -3,8 +3,19 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   margin: 32px 0 40px;
+  display: flex;
+  flex-wrap: wrap;
 
   button {
+    margin-top: 8px;
     margin-right: 8px;
+  }
+
+  @media (max-width: 400px) {
+    flex-direction: column;
+    
+    button {
+      width: 100%;
+    }
   }
 `
