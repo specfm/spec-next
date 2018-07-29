b027868ea8d8c2223cd5452fe48365c169ac8731
Brian Lovin <briandlovin@gmail.com>
2018-07-29 09:07:21 -0700

    Refactor simplecast api request to be a bit more generi

--- servers/api/index.js
+++ servers/api/index.js
@@ -2,26 +2,23 @@
 require('now-env')
 const microCors = require('micro-cors')
 const { send } = require('micro')
-const fetch = require('node-fetch');
 const cache = require('micro-cacheable')
-const API_URL_ROOT = 'https://api.simplecast.com/v1'
-const API_KEY = process.env.SIMPLECAST_API_KEY
 const cors = microCors()
+const getStats = require('./getStats')
+const simplecast = require('./simplecast')
 
 const handler = async (req, res) => {
   const sendError = (code = 400, error = 'Invalid url') => send(res, code, { error })
   if (!req.url) return sendError()
-  if (!req.url.startsWith('/podcasts')) return sendError()
   if (req.url.length === 0 || req.url === '/') return sendError()
 
-  const response = await fetch(`${API_URL_ROOT}${req.url}`, {
-    method: "GET",
-    headers: {
-      "X-API-KEY": API_KEY
-    }
-  });
-	const json = await response.json();
-	return json;
+  if (req.url.startsWith('/stats')) {
+    return await getStats(req.url)
+  }
+
+  if (!req.url.startsWith('/podcasts')) return sendError()
+
+  return await simplecast(req.url)
 };
 
 const result = process.env.NODE_ENV === 'production'
