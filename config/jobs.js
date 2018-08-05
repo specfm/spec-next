8d43b26724fb1fc2863ec5663c6b3af459724f5f
Brian Lovin <briandlovin@gmail.com>
2018-08-05 14:31:02 -0700

    Fix flow

--- config/jobs.js
+++ config/jobs.js
@@ -2,13 +2,13 @@
 
 export default [
   {
-    id: 1,
+    id: "1",
     job_title: "Senior Software Engineer",
     job_description: "",
     job_location: "New York City",
     job_application_link: "https://www.reaktor.com/careers/senior-software-engineer/?utm_source=SpecFM&utm_campaign=2018",
-    creation_date: new Date(),
-    end_date: new Date(),
+    creation_date: JSON.stringify(new Date()),
+    end_date: JSON.stringify(new Date()),
     job_link: "https://www.reaktor.com/careers/senior-software-engineer/?utm_source=SpecFM&utm_campaign=2018",
     company: {
       name: "Reaktor",
@@ -17,13 +17,13 @@ export default [
   },
 
   {
-    id: 2,
+    id: "2",
     job_title: "Lead Product Designer",
     job_description: "",
     job_location: "New York City",
     job_application_link: "https://www.reaktor.com/careers/product-designer-for-reaktor-new-york/?utm_source=SpecFM&utm_campaign=2018",
-    creation_date: new Date(),
-    end_date: new Date(),
+    creation_date: JSON.stringify(new Date()),
+    end_date: JSON.stringify(new Date()),
     job_link: "https://www.reaktor.com/careers/product-designer-for-reaktor-new-york/?utm_source=SpecFM&utm_campaign=2018",
     company: {
       name: "Reaktor",
@@ -32,13 +32,13 @@ export default [
   },
 
   {
-    id: 3,
+    id: "3",
     job_title: "Product Designer",
     job_description: "",
     job_location: "Remote",
     job_application_link: "https://grnh.se/e10ec8691",
-    creation_date: new Date(),
-    end_date: new Date(),
+    creation_date: JSON.stringify(new Date()),
+    end_date: JSON.stringify(new Date()),
     job_link: "https://grnh.se/e10ec8691",
     company: {
       name: "InVision",
@@ -47,13 +47,13 @@ export default [
   },
 
   {
-    id: 4,
+    id: "4",
     job_title: "Design Specialist",
     job_description: "",
     job_location: "Remote",
     job_application_link: "https://boards.greenhouse.io/invision/jobs/867187",
-    creation_date: new Date(),
-    end_date: new Date(),
+    creation_date: JSON.stringify(new Date()),
+    end_date: JSON.stringify(new Date()),
     job_link: "https://boards.greenhouse.io/invision/jobs/867187",
     company: {
       name: "InVision",
@@ -62,13 +62,13 @@ export default [
   },
 
   {
-    id: 5,
+    id: "5",
     job_title: "Senior Manager, Design Education",
     job_description: "",
     job_location: "Remote",
     job_application_link: "https://boards.greenhouse.io/invision/jobs/1206168",
-    creation_date: new Date(),
-    end_date: new Date(),
+    creation_date: JSON.stringify(new Date()),
+    end_date: JSON.stringify(new Date()),
     job_link: "https://boards.greenhouse.io/invision/jobs/1206168",
     company: {
       name: "InVision",
@@ -77,13 +77,13 @@ export default [
   },
 
   {
-    id: 6,
+    id: "6",
     job_title: "Software Engineer - Compute",
     job_description: "",
     job_location: "New York, Paris, Remote",
     job_application_link: "https://www.datadoghq.com/careers/detail/?gh_jid=847188",
-    creation_date: new Date(),
-    end_date: new Date(),
+    creation_date: JSON.stringify(new Date()),
+    end_date: JSON.stringify(new Date()),
     job_link: "https://www.datadoghq.com/careers/detail/?gh_jid=847188",
     company: {
       name: "Datadog",
