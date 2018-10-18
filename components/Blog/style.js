affca11fa57ce80573a0964693a89027e32d5fed
Brian Lovin <briandlovin@gmail.com>
2018-10-17 21:51:57 -0700

    Small contrast improvements

--- components/Blog/style.js
+++ components/Blog/style.js
@@ -38,7 +38,7 @@ export const Notice = styled.div`
 `
 
 export const NoticeTitle = styled.h6`
-  color: ${props => tint(props.theme.bg.wash, -80)};
+  color: ${props => tint(props.theme.bg.wash, -88)};
   font-weight: 700!important;
   text-transform: uppercase;
   font-size: 16px;
@@ -50,11 +50,11 @@ export const NoticeTitle = styled.h6`
 
 export const NoticeDescription = styled.p`
   margin-top: 12px;
-  color: ${props => tint(props.theme.bg.wash, -64)};
+  color: ${props => tint(props.theme.bg.wash, -72)};
   font-weight: 400;
 
   a {
-    color: ${props => tint(props.theme.bg.wash, -80)};
+    color: ${props => tint(props.theme.bg.wash, -88)};
     font-weight: 600;
   }
 
