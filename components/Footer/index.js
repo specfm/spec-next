5155682d696d0ce44d2e01a4702ac6b863f8a3e8
Brian Lovin <briandlovin@gmail.com>
2018-10-18 10:30:23 -0700

    Add conditional footer margin based on global player

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,29 +2,37 @@
 import * as React from 'react'
 import { Container, Description, Icons } from './style'
 import Icon from '../Icon'
+import GlobalPlayer from '../GlobalPlayer/context'
 
 class Footer extends React.Component<{}> {
   render() {
-    return ( 
-      <Container>
-        <Icons>
-          <a href="https://twitter.com/specfm" target="_blank" rel="noopener noreferrer">
-            <Icon glyph={'twitter'} />
-          </a>
-          
-          <a href="https://github.com/specfm/spec-next" target="_blank" rel="noopener noreferrer">
-            <Icon glyph={'github'} />
-          </a>
+    return (
+      <GlobalPlayer.Consumer>
 
-          <a href="https://spectrum.chat/specfm" target="_blank" rel="noopener noreferrer">
-            <Icon glyph={'spectrum-solid'} />
-          </a>
-        </Icons>
-        
-        <Description>
-          Copyright {new Date().getFullYear()} Spec Network, Inc.
-        </Description>
-      </Container>
+        {
+          context => (
+            <Container addPlayerPadding={context.trackQueue.length > 0}>
+              <Icons>
+                <a href="https://twitter.com/specfm" target="_blank" rel="noopener noreferrer">
+                  <Icon glyph={'twitter'} />
+                </a>
+                
+                <a href="https://github.com/specfm/spec-next" target="_blank" rel="noopener noreferrer">
+                  <Icon glyph={'github'} />
+                </a>
+
+                <a href="https://spectrum.chat/specfm" target="_blank" rel="noopener noreferrer">
+                  <Icon glyph={'spectrum-solid'} />
+                </a>
+              </Icons>
+              
+              <Description>
+                Copyright {new Date().getFullYear()} Spec Network, Inc.
+              </Description>
+            </Container>
+         )
+        }
+      </GlobalPlayer.Consumer>
     )
   }
 }
