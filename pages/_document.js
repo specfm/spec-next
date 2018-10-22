89bff389999b2bac877d5f4f4ad00785dea54e6d
Brian Lovin <briandlovin@gmail.com>
2018-10-21 23:20:33 -0700

    Include global component

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,7 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <Main />
+          <GlobalStyles />
           <NextScript />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
