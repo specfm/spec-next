8d67ca275b3b1288f1b64b87cc8c4ee4deb4d042
Brian Lovin <briandlovin@gmail.com>
2019-11-24 21:49:58 -0500

    Fix tests

--- src/lib/scToConfigPodcasts.js
+++ src/lib/scToConfigPodcasts.js
@@ -4,11 +4,11 @@ import type { ConfigPodcast } from '../../types';
 export default (array: Array<ConfigPodcast>) => {
   let podcasts = array;
 
-  const active = ['363', '1034', '9926', '6265', '1386', '1684', '2128', '2693', '4211', '99999'];
+  const active = [363, 1034, 9926, 6265, 1386, 1684, 2128, 2693, 4211, 99999];
 
   if (array && array.length > 0) {
     const sc = array;
-    const sorted = [...active, '1332', '1457', '2070'].map(id =>
+    const sorted = [...active, 1332, 1457, 2070].map(id =>
       sc.find(podcast => podcast && podcast.id === id)
     );
 
