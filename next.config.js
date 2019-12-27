81278c58b738fc23ce70eeeb0f7fe1956544e5dd
Brian Lovin <brianlovin@github.com>
2019-12-27 09:09:50 -0500

    Fix images not loading

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   target: 'serverless',
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty',
-    };
-
-    return config;
-  },
   env: {
     "SIMPLECAST_API_KEY": process.env.SIMPLECAST_API_KEY
   }
