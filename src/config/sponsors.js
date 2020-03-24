a4210bdbe72aa83107cd93f81dc76f4b74f7cac0
Brian Lovin <briandlovin@gmail.com>
2020-03-24 11:22:48 -0700

    Update sponsors.js

--- src/config/sponsors.js
+++ src/config/sponsors.js
@@ -583,4 +583,10 @@ export default [
       'https://landing.prodperfect.com/tea.html?utm_source=tea&utm_medium=advertisement&utm_campaign=core_demo_request',
     logoUrl: '/static/img/sponsors/ProdPerfect.png',
   },
+  {
+    name: 'ProdPerfect',
+    url:
+      'https://landing.prodperfect.com/tea.html?utm_source=tea&utm_medium=advertisement&utm_campaign=core_demo_request',
+    logoUrl: '/static/img/sponsors/ProdPerfect.png',
+  },
 ];
