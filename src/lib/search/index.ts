052a42cc3ed6ffb7b29f7a43be2608f134cb0275
Brian Lovin <briandlovin@gmail.com>
2020-06-02 08:02:55 -0600

    Add error tracing to search API route

--- src/lib/search/index.ts
+++ src/lib/search/index.ts
@@ -1,15 +1,17 @@
 import indexPodcasts from '~/lib/search/utils/indexPodcasts'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { sentryAPIHandler } from '../sentry'
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { token: reqToken } = req.query
-  const token = process.env.SEARCH_INDEXING_TOKEN
+export default sentryAPIHandler(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    const { token: reqToken } = req.query
+    const token = process.env.SEARCH_INDEXING_TOKEN
 
-  if (!token || !reqToken || token !== reqToken) {
-    return res.status(500).json({ error: 'Invalid token' })
+    if (!token || !reqToken || token !== reqToken) {
+      return res.status(500).json({ error: 'Invalid token' })
+    }
+
+    await indexPodcasts()
+    return res.status(200).json({ status: 'Indexing complete!' })
   }
-
-  await indexPodcasts()
-
-  return res.status(200).json({ status: 'Indexing complete!' })
-}
+)
--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -1,7 +1,21 @@
 import * as Sentry from '@sentry/node'
+import { NextApiRequest, NextApiResponse, NextApiHandler } from 'next'
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
 })
+
+export function sentryAPIHandler(apiHandler: NextApiHandler) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      return await apiHandler(req, res)
+    } catch (error) {
+      console.error({ error })
+      Sentry.captureException(error)
+      await Sentry.flush(2000)
+      return res.status(500).json({ error })
+    }
+  }
+}
 
 export default Sentry
