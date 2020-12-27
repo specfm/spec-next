7ac8b35fb2789cfe39143fbf74410d5b24d4deb7
Brian Lovin <briandlovin@gmail.com>
2020-12-27 10:55:00 -0700

    Update about and sponsors page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,36 +1,9 @@
 import * as React from 'react'
-import { Container, Description, Icons } from './style'
-import Icon from '../Icon'
+import { Container, Description } from './style'
 
 export default function Footer() {
   return (
     <Container>
-      <Icons>
-        <a
-          href="https://twitter.com/specfm"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Icon glyph="twitter" />
-        </a>
-
-        <a
-          href="https://github.com/specfm/spec-next"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Icon glyph="github" />
-        </a>
-
-        <a
-          href="https://spectrum.chat/specfm"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Icon glyph="spectrum-solid" />
-        </a>
-      </Icons>
-
       <Description>
         Copyright {new Date().getFullYear()} Spec Network, Inc.
       </Description>
